b947fd7cd16b27e1388e0e62fdc568114ce0ca32
linorwang <17005041213@163.com>
2025-10-25 00:21:19 +0800

    feat: 新增包含函数

--- slice/contains.go
+++ slice/contains.go
@@ -16,3 +16,14 @@ func ContainsFunc[T any](src []T, equal func(src T) bool) bool {
 	}
 	return false
 }
+
+// ContainsAny 判断 src 里面是否存在 dst 中的任何一个元素
+func ContainsAny[T comparable](src, dst []T) bool {
+	srcMap := toMap[T](src)
+	for _, v := range dst {
+		if _, exist := srcMap[v]; exist {
+			return true
+		}
+	}
+	return false
+}
