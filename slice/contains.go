87608385ce97bc6c8fcee4b7f430863f6b40ba74
linorwang <17005041213@163.com>
2025-10-25 00:41:13 +0800

    feat: 添加包含所有元素

--- slice/contains.go
+++ slice/contains.go
@@ -27,3 +27,14 @@ func ContainsAny[T comparable](src, dst []T) bool {
 	}
 	return false
 }
+
+// ContainsAll 判断 src 里面是否存在 dst 中的所有元素
+func ContainsAll[T comparable](src, dst []T) bool {
+	srcMap := toMap[T](src)
+	for _, v := range dst {
+		if _, exist := srcMap[v]; !exist {
+			return false
+		}
+	}
+	return true
+}
